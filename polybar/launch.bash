05eb12916c4ea13f2b150c6cef5f0cb517ba8712
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2021-12-29 00:52:15 +0530

    fixed and relativised color variables

--- polybar/launch.bash
+++ polybar/launch.bash
@@ -1,15 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
+config="polybar -q -c ~/Dotfiles/syntax-dots/polybar/config.ini"
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar example &
+eval "$config main-bar" & disown
 
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
 if [[ $my_laptop_external_monitor = *connected* ]]; then
-    polybar example_extrnl &
+	eval "$config external-bar" & disown
 fi
+
+# vim:ft=bash:nowrap
